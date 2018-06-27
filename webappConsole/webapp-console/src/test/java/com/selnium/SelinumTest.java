022491932e8bebeb0dba21d2ddbfe08bb06a895f
pallavipatil66 <36727171+pallavipatil66@users.noreply.github.com>
2018-06-27 13:25:40 +0530

    Check for selinum

--- webappConsole/webapp-console/src/test/java/com/selnium/SelinumTest.java
+++ webappConsole/webapp-console/src/test/java/com/selnium/SelinumTest.java
@@ -29,13 +29,14 @@ public class SelinumTest {
 			passwordBox.sendKeys("admin");
 			WebElement loginButton = driver.findElement(By.name("login"));
 			loginButton.click();
-			WebElement userMan = driver.findElement(By.linkText("User Management"));
+			/*WebElement userMan = driver.findElement(By.linkText("User Management"));
 			Actions action = new Actions(driver);
 			action.moveToElement(userMan).build().perform();
 			WebElement userButton = driver.findElement(By.linkText("User"));
 			System.out.println("Clicked on User ");
 			userButton.click();
 			Thread.sleep(5000);
+*/			
 			System.out.println(driver.getTitle());
 			driver.close();
 
