f4aeca1a3da858242bcd7ee9fcf732341bb0bae0
pallavipatil66 <36727171+pallavipatil66@users.noreply.github.com>
2018-10-15 15:20:08 +0530

    Jenkins Testing

--- webappConsole/webapp-console/src/main/java/com/capgemini/webapp/spring/controller/data/ModelDataController.java
+++ webappConsole/webapp-console/src/main/java/com/capgemini/webapp/spring/controller/data/ModelDataController.java
@@ -334,7 +334,7 @@ public class ModelDataController {
 	@RequestMapping(value = "/bookingRequest/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> processBookingRequest(@RequestBody CustomerBookingModel bookingModel) {
 
-		logger.debug("Process booking Request1");
+		logger.debug("Process booking Request3");
 		
 		boolean isQuotationCreated = bookingService.processBookingRequest(bookingModel);
 		
